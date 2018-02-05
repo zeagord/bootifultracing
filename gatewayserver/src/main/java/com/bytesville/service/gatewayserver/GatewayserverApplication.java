d9a5da563e6144527dd983d2e78690ddd3f84da1
Raja Sundaram Ganesan <raja.gan@AGC01410.local>
2018-02-05 15:35:37 +0700

    Updated to Spring boot 2

--- gatewayserver/src/main/java/com/bytesville/service/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/bytesville/service/gatewayserver/GatewayserverApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableEurekaClient
 public class GatewayserverApplication {
 
 	public static void main(String[] args) {
--- zipkinserver/src/main/java/com/bytesville/tracing/zipkinserver/ZipkinServerApplication.java
+++ zipkinserver/src/main/java/com/bytesville/tracing/zipkinserver/ZipkinServerApplication.java
@@ -2,12 +2,10 @@ package com.bytesville.tracing.zipkinserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableZipkinServer
-@EnableDiscoveryClient
 public class ZipkinServerApplication {
 
 	public static void main(String[] args) {
